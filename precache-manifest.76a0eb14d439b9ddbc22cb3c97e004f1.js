6d0f82f68a6e28207387ac66559758c790e51c86
Duncan Gibbs <duncgibbs@gmail.com>
2020-02-23 23:15:36 -0600

    Updates

---
+++ precache-manifest.76a0eb14d439b9ddbc22cb3c97e004f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5c10dc1c0df972cb9b43a108fa41ed",
+    "url": "/board-game-collection-visualizer/index.html"
+  },
+  {
+    "revision": "b8130654ecb8eef52003",
+    "url": "/board-game-collection-visualizer/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fe6a7dcb3c3251994c7f",
+    "url": "/board-game-collection-visualizer/static/js/2.c607d2fe.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/board-game-collection-visualizer/static/js/2.c607d2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8130654ecb8eef52003",
+    "url": "/board-game-collection-visualizer/static/js/main.0740cc24.chunk.js"
+  },
+  {
+    "revision": "7f16924bfcc417f98bc7",
+    "url": "/board-game-collection-visualizer/static/js/runtime-main.9308b68e.js"
+  }
+]);
